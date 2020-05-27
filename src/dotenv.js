ae8d023134824a5bd4c70d100c76ec269acd8d04
Wil Wilsman <wil@wilwilsman.com>
2020-05-27 15:37:17 -0500

    ✨ Allow disabling dotenv files

--- src/dotenv.js
+++ src/dotenv.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 // mimic dotenv-rails file hierarchy
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
 export function config() {
-  let {NODE_ENV: env} = process.env;
+  let {NODE_ENV: env, PERCY_DISABLE_DOTENV: disable} = process.env;
+
+  // don't load dotenv files when disabled
+  if (disable) return;
 
   let paths = [
     env && `.env.${env}.local`,
